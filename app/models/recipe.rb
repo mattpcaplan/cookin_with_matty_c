4c6b36ed9081ea55d78b71d0ba0cac2d111a3b69
Matt Caplan <mattpcaplan@gmail.com>
2017-05-02 22:34:30 -0400

    added associations.  finished up final steps

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,6 +4,6 @@ class Recipe < ApplicationRecord
   accepts_nested_attributes_for :ingredients, reject_if: :all_blank, allow_destroy: true
   accepts_nested_attributes_for :directions, reject_if: :all_blank, allow_destroy: true
   validates :title, :description, :image, presence: true
-  has_attached_file :image, styles: { medium: "400x400#" }, default_url: "/images/:style/missing.png"
+  has_attached_file :image, styles: { medium: "400x400" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
